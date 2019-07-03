deef139be480126dc4137f32590e268e0ad1ff31
Omer <brozkiomer@gmail.com>
2019-07-03 06:47:04 +0300

    Added block testing file

--- src/test/js/blockchain/block.test.js
+++ src/test/js/blockchain/block.test.js
@@ -1,2 +1,18 @@
 describe('Block', () => {
+
+  const previousBlockHash = 'prev-hash';
+  const index = 'index';
+  const previousProof = 'bar-hash';
+  const timestamp = 'date';
+  const transactions = ['blockchain', 'data'];
+  const block = new Block(index, previousBlockHash, previousProof, transactions);
+  
+
+  it('has a timestamp, lastHash, hash, and data property', () => {
+    expect(block.previousBlockHash).toEqual(previousBlockHash);
+    expect(block.index).toEqual(index);
+    expect(block.previousProof).toEqual(previousProof);
+    expect(block.timestamp).toEqual(timestamp);
+    expect(block.transactions).toEqual(transactions);
+  });
 });
