66df0678f2283ea2beb91452a38a971d6c11861c
Vlad <vjidkov@uottawa.ca>
2019-07-11 19:02:08 -0400

    todo later

--- src/main/js/client/wallet.js
+++ src/main/js/client/wallet.js
@@ -1,29 +1,17 @@
-const { generateKeyPair, createSign, createVerify } = require('crypto');
+const { generateKeyPair, createSign, createVerify createDiffieHellman} = require('crypto');
 const promise = require('promise');
+const util = require('util');
 
 class Wallet {
     constructor() {
         const self = this;
-        generateKeyPair('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: {
-                type: 'spki',
-                format: 'pem'
-            },
-            privateKeyEncoding: {
-                type: 'pkcs8',
-                format: 'pem',
-                cipher: 'aes-256-cbc',
-                passphrase: (Math.random() / Math.random() * Date.now()).toString()
-            }
-        }, (err, publicKey, privateKey) => {
-            console.log(publicKey);
-            console.log(privateKey);
-            self.publicKey = publicKey;
-            self.privateKey = privateKey;
-        });
+        var diffieHellman = createDiffieHellman(2048)
     }
 
+    getPublicKey() {
+        return this.publicKey;
+    }
 }
-var wallet = new Wallet();
-console.log(wallet.publicKey);
+
+
+
